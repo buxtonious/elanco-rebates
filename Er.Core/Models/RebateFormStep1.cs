fcf71047cd3614ba07dc016b7d5bae9c133ca517
Harry Buxton <hb740@outlook.com>
2023-12-11 22:39:02 +0000

    Switch to jquery validation

--- Er.Core/Models/RebateFormStep1.cs
+++ Er.Core/Models/RebateFormStep1.cs
@@ -1,5 +1,6 @@
 ﻿using Er.Models.Entities;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Er.Core.Models
 {
@@ -17,36 +18,57 @@ namespace Er.Core.Models
             Countries = countries;
         }
 
+        [Required(ErrorMessage = "Title is required")]
         [DisplayName("Title")]
         public string Salutation { get; set; }
 
+        [Required(ErrorMessage = "First name is required")]
+        [MaxLength(50, ErrorMessage = "First name exceeds maximum length")]
         [DisplayName("First name")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required")]
+        [MaxLength(50, ErrorMessage = "Last name exceeds maximum length")]
         [DisplayName("Last name")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
+        [MaxLength(50, ErrorMessage = "Email exceeds maximum length")]
+        [EmailAddress(ErrorMessage = "Email is not a valid email address")]
         [DisplayName("Email")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Address line 1 is required")]
+        [MaxLength(128, ErrorMessage = "Address line 1 exceeds maximum length")]
         [DisplayName("Address line 1")]
         public string PersonalAddressLine1 { get; set; }
 
+        [MaxLength(128, ErrorMessage = "Address line 2 exceeds maximum length")]
         [DisplayName("Address line 2 (optional)")]
         public string PersonalAddressLine2 { get; set; }
 
+        [Required(ErrorMessage = "Address city is required")]
+        [MaxLength(50, ErrorMessage = "Address city exceeds maximum length")]
         [DisplayName("Address city")]
         public string PersonalAddressCity { get; set; }
 
+        [Required(ErrorMessage = "Address country is required")]
         [DisplayName("Address country")]
         public string PersonalAddressCountry { get; set; }
 
+        [Required(ErrorMessage = "Address postcode is required")]
+        [MaxLength(16, ErrorMessage = "Address postcode exceeds maximum length")]
+        [RegularExpression(@"^([A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$", ErrorMessage = "Address postcode is not a valid UK postcode")]
         [DisplayName("Address postcode")]
         public string PersonalAddressPostCode { get; set; }
 
+        [Required(ErrorMessage = "Pet name is required")]
+        [MaxLength(50, ErrorMessage = "Pet name exceeds maximum length")]
         [DisplayName("Pet name")]
         public string PetName { get; set; }
 
+        [MaxLength(50, ErrorMessage = "Pet birthday exceeds maximum length")]
+        [RegularExpression(@"^(0[1-9]|[1-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/\d{4}$", ErrorMessage = "Pet birthday is not a valid birthday")]
         [DisplayName("Pet birthday (optional)")]
         public string PetBirthday { get; set; }
 
--- Er.Web/wwwroot/js/main.js
+++ Er.Web/wwwroot/js/main.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const validator = new Validator();
-    const navigationManager = new NavigationManager(validator);
+    const navigationManager = new NavigationManager();
 
     registerNavigationButtons();
     registerSubmissionButton();
-    registerFields();
 
     function registerNavigationButtons() {
         let nextButtons = document.querySelectorAll(".form-navigation .next");
@@ -34,48 +32,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
         form.addEventListener("submit", async function (event) {
             event.preventDefault();
-
-            let currentStep = event.submitter.closest(".step").dataset.step;
             await navigationManager.submitForm(currentStep);
         });
-    }
-
-    function registerFields() {
-        let inputs = document.querySelectorAll('input');
-        let selects = document.querySelectorAll('select');
-        let fields = [...inputs, ...selects];
-
-        for (let i = 0; i < fields.length; i++) {
-            fields[i].addEventListener("change", function (event) {
-                validator.clearValidationError(event.target);
-            });
-        }
     }
 });
 
 class NavigationManager {
-    validator;
+    async submitForm() {
+        var form = $("form");
+        form.validate();
 
-    constructor(validator) {
-        this.validator = validator;
-    }
-
-    async submitForm(currentStep) {
-        this.validator = new Validator(currentStep);
-        let isValid = this.validator.validate();
-
-        if (!isValid) {
+        if (!form.valid()) {
             return;
         }
 
-        let form = document.querySelector("form");
-        let formData = new FormData(form);
         let response;
 
         try {
             response = await fetch("/Rebate/Submit", {
                 method: "POST",
-                body: formData
+                body: form.serialize()
             });
         } catch (error) {
             console.log(error);
@@ -89,10 +65,10 @@ class NavigationManager {
     }
 
     navigateForward(currentStep, stepToNavigateTo) {
-        this.validator = new Validator(currentStep);
-        let isValid = this.validator.validate();
+        var form = $("form");
+        form.validate();
 
-        if (!isValid) {
+        if (!form.valid()) {
             return;
         }
 
@@ -121,199 +97,5 @@ class NavigationManager {
 
         currentStepContainer.style.display = "none";
         previousStepContainer.style.display = "block";
-    }
-}
-
-class Validator {
-    currentStep;
-    validationErrors;
-
-    constructor(currentStep) {
-        this.currentStep = currentStep;
-        this.validationErrors = [];
-    }
-
-    validate() {
-        this.clearValidationErrors();
-
-        // find all required inputs and selects then merge into one array so we only need to create one loop
-        let requiredInputs = document.querySelectorAll(`.step[data-step="${this.currentStep}"] input[required]`);
-        let requiredSelects = document.querySelectorAll(`.step[data-step="${this.currentStep}"] select[required]`);
-        let requiredFields = [...requiredInputs, ...requiredSelects];
-
-        for (let i = 0; i < requiredFields.length; i++) {
-            // checks the validity of the field using form validation
-            let isFieldValid = requiredFields[i].checkValidity();
-
-            if (isFieldValid) {
-                continue;
-            }
-
-            // create the error message using the label name (e.g. first name, email)
-            let labelText = requiredFields[i].labels[0].textContent;
-            let errorMessage = this.createErrorMessage(requiredFields[i], labelText, false);
-            let anchorErrorMessage = this.createErrorMessage(requiredFields[i], labelText, true);
-
-            // add the message to the validation errors, we also add a key which is the input ID
-            // so we have an easier time removing errors from the array and validation summary we create later
-            this.validationErrors.push({ key: requiredFields[i].id, message: errorMessage, anchorMessage: anchorErrorMessage});
-            this.createValidationElement(requiredFields[i], errorMessage);
-        }
-
-        if (!this.validationErrors || this.validationErrors.length > 0) {
-            this.createValidationSummary();
-
-            return false;
-        }
-
-        return true;
-    }
-
-    displayErrors(validationErrors) {
-        this.clearValidationErrors();
-
-        // displays server side validation errors
-        for (let i = 0; i < validationErrors.length; i++) {
-            let fieldId = validationErrors[i].field.replace(/\./g, "_");;
-            let errorMessage = validationErrors[i].message;
-
-            let field = document.getElementById(fieldId);
-
-            this.validationErrors.push({ key: fieldId, message: errorMessage });
-            this.createValidationElement(field, errorMessage);
-        }
-
-        this.createValidationSummary();
-    }
-
-    createErrorMessage(requiredField, labelText, attachAnchorTag) {
-        labelText = labelText.toLowerCase();
-
-        if (attachAnchorTag) {
-            labelText = `<a href="#${requiredField.id}">${labelText}</a>`;
-        }
-
-        let errorMessage = `Please enter your ${labelText}`;
-
-        // uses form validation to determine what validation issue has occured
-        // if none match we use the default message above
-        if (requiredField.type == "select-one") {
-            errorMessage = `Please select your ${labelText}`;
-        } else if (requiredField.type == "radio") {
-            errorMessage = `Please select a ${labelText}`;
-        } else if (requiredField.type == "checkbox") {
-            errorMessage = `Please tick ${labelText}`;
-        }
-
-        if (requiredField.validity.typeMismatch) {
-            errorMessage = `Please enter ${labelText} in the correct format`;
-        }
-
-        return errorMessage;
-    }
-
-    createValidationElement(requiredField, errorMessage) {
-        let inputContainer = requiredField.closest("div");
-        let errorSpan = document.createElement("span");
-
-        errorSpan.classList.add("validation-error", "d-block");
-        errorSpan.textContent = errorMessage;
-
-        requiredField.classList.add("validation-error");
-        inputContainer.appendChild(errorSpan);
-    }
-
-    createValidationSummary() {
-        if (!this.validationErrors || this.validationErrors.length == 0) {
-            return;
-        }
-
-        let validationSummaryElement = document.querySelector(".validation-summary");
-        let alertElement = document.createElement("div");
-        let alertHeading = document.createElement("h5");
-        let alertUnorderedList = document.createElement("ul");
-
-        alertElement.classList.add("alert", "alert-danger");
-        alertElement.setAttribute("role", "alert");
-
-        alertHeading.classList.add("alert-heading", "fw-bold");
-        alertHeading.textContent = "Please enter something in these field(s):";
-
-        for (let i = 0; i < this.validationErrors.length; i++) {
-            let alertListItem = document.createElement("li");
-
-            alertListItem.classList.add(this.validationErrors[i].key);
-            alertListItem.innerHTML = this.validationErrors[i].anchorMessage;
-
-            alertUnorderedList.appendChild(alertListItem);
-        }
-
-        alertElement.appendChild(alertHeading);
-        alertElement.appendChild(alertUnorderedList);
-
-        validationSummaryElement.appendChild(alertElement);
-
-        window.scrollTo(0, 0);
-    }
-
-    clearValidationError(field) {
-        let key = field.id;
-
-        for (let i = 0; i < this.validationErrors.length; i++) {
-            if (this.validationErrors[i].key == key) {
-                this.validationErrors.splice(i, 1);
-
-                break;
-            }
-        }
-
-        let list = document.querySelector(".validation-summary ul");
-        let listItem = document.querySelector(`.validation-summary li.${key}`);
-        let errorSpan = field.closest("div").querySelector("span.validation-error");
-
-        if (listItem && errorSpan) {
-            listItem.remove();
-            errorSpan.remove();
-
-            field.classList.remove("validation-error");
-        }
-
-        if (list && !list.hasChildNodes()) {
-            let alert = document.querySelector(".validation-summary .alert");
-
-            alert.remove();
-            this.validationErrors = [];
-        }
-    }
-
-    clearValidationErrors() {
-        this.validationErrors = [];
-
-        let spanElements = document.querySelectorAll("span.validation-error");
-        let inputElements = document.querySelectorAll("input.validation-error");
-        let selectElements = document.querySelectorAll("select.validation-error");
-        let alertElement = document.querySelector('.validation-summary .alert');
-
-        if (spanElements && spanElements.length > 0) {
-            for (let i = 0; i < spanElements.length; i++) {
-                spanElements[i].remove();
-            }
-        }
-
-        if (inputElements && inputElements.length > 0) {
-            for (let i = 0; i < inputElements.length; i++) {
-                inputElements[i].classList.remove("validation-error");
-            }
-        }
-
-        if (selectElements && selectElements.length > 0) {
-            for (let i = 0; i < selectElements.length; i++) {
-                selectElements[i].classList.remove("validation-error");
-            }
-        }
-
-        if (alertElement) {
-            alertElement.remove();
-        }
     }
 }
